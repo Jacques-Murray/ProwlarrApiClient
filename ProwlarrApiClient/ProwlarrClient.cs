8a7d0cef7211d597ad2f36ae07fdc7b4fe3a768a
JacquesMurray <jacquesmmurray@gmail.com>
2025-04-01 16:18:20 +0200

    Refactor using directives in SystemStatus and ProwlarrClient
    Removed unnecessary using directives in `SystemStatus.cs` and `ProwlarrClient.cs` to streamline code. Retained only essential directives, focusing on JSON serialization and the use of models from the `ProwlarrApiClient.Models` namespace.
    
    Cleanup unused namespaces in SystemStatus and ProwlarrClient
    
    Removed several unnecessary `using` directives from `SystemStatus.cs` and `ProwlarrClient.cs` to streamline the code. Added `using ProwlarrApiClient.Models;` in `ProwlarrClient.cs` to utilize models from the appropriate namespace.

--- ProwlarrApiClient/ProwlarrClient.cs
+++ ProwlarrApiClient/ProwlarrClient.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using ProwlarrApiClient.Models;
 using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace ProwlarrApiClient
 {
